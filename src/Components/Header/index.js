304a778914d3e3485af145ecfbecf076a1170d8c
jordanjpmg <jordanjpmg@gmail.com>
2020-03-25 17:23:44 -0300

    Menu Criado

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Logo from "../../objects/Logo";
 import Sobre from "../../objects/Sobre";
+import Menu from "../../objects/Menu";
 import "./styles.css";
 
 const Header = () => {
@@ -8,6 +9,7 @@ const Header = () => {
     <header className="Container-header">
       <Logo />
       <Sobre />
+      <Menu />
     </header>
   );
 };
---
+++ src/objects/Menu/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+const Menu = () => {
+  return (
+    <a className="menu" href="to-do">
+      <span className="center">Menu</span>
+    </a>
+  );
+};
+
+export default Menu;
