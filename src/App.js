10e80d5cf7330d4de6370dedf6f62cc78f1c82d2
jordanjpmg <jordanjpmg@gmail.com>
2020-03-27 12:33:29 -0300

    Object Player Criado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
 import Board from "./components/Board";
+import Player from "./objects/Player"
 function App() {
   return (
     <>
       <Header />
       <Board />
+      <Player/>
     </>
   );
 }
---
+++ src/objects/Player/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./styles.css"
+const Player = ()=>{
+  return(<button className= "player">X</button>)
+}
+
+export default Player
