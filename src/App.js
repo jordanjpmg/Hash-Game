1e00b6637ae79319d833fc1a822877236d0f2253
jordanjpmg <jordanjpmg@gmail.com>
2020-03-28 17:00:34 -0300

    Alinhando o componente HashTag ao centro da tela

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
+import "./App.css";
 import Header from "./components/Header";
 import HashTag from "./components/HashTag";
 
 function App() {
   return (
-    <>
+    <main className="app">
       <Header />
-      <HashTag/>
-
-    </>
+      <HashTag />
+    </main>
   );
 }
 
