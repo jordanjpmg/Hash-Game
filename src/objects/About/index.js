6af993d6587c4998701a2074bedb77e23cd3853a
jordanjpmg <jordanjpmg@gmail.com>
2020-03-30 13:19:01 -0300

    Estilizacao do Object About criado

--- src/objects/About/index.js
+++ src/objects/About/index.js
@@ -1,7 +1,7 @@
 import React from "react";
-
+import "./styles.css";
 const About = () => {
-  return <article>About</article>;
+  return <article className="about"></article>;
 };
 
 export default About;
