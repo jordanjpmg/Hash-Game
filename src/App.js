6513c426b881afc3ce51c58e24f8d50df9c197bb
jordanjpmg <jordanjpmg@gmail.com>
2020-03-30 13:13:04 -0300

    Object About criado

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./components/Header";
 import HashTag from "./components/HashTag";
 import CheckBox from "./objects/CheckBox";
+import About from "./objects/About";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         type="checkbox"
         content="Mostrar eventos"
       />
+      <About />
     </main>
   );
 }
