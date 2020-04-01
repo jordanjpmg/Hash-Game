52157ecf9623bbdf1e92fc37af1840a2c3f174ed
jordanjpmg <jordanjpmg@gmail.com>
2020-04-01 18:06:40 -0300

    Refactoring do Logo paga LogoCollab

--- src/objects/Logo/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import CollabCode from "../../images/logo.svg";
-import "./styles.css";
-const Logo = () => {
-  return <img src={CollabCode} alt="Logo CollabCode"></img>;
-};
-
-export default Logo;
---
+++ src/objects/LogoCollab/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import CollabCode from "../../images/logo.svg";
+
+const LogoCollab = () => {
+  return (
+    <img className="logo-collab" src={CollabCode} alt="Logo CollabCode"></img>
+  );
+};
+
+export default LogoCollab;
